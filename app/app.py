5e14f81f35e5e1d605c196912bf7a4b3cfc40713
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-04-27 00:14:44 -0600

    first commit

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+
+    data = {
+        'titulo': 'Listado',
+        'encabezado' : 'Listado de cursos'
+
+    }
+
+    return render_template('index.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
